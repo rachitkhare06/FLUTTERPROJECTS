c2ed5d4e928c18ff222efd6fe9f9314cfc818abb
RACHIT <rachit.vansh06@gmail.com>
2022-07-26 16:03:46 +0530

    update

---
+++ quizzler-flutter/README.md
@@ -0,0 +1,3 @@
+Quizzler - A True/False Quiz App
+• Implemented object oriented programming in dart language.
+• Deployed the app on a physical device.
---
+++ quizzler-flutter/android/app/src/main/java/co/appbrewery/quizzler/MainActivity.java
@@ -0,0 +1,9 @@
+package co.appbrewery.quizzler;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+
+public class MainActivity extends FlutterActivity {
+
+  }
+
